a3a0e790790a2c6ea2b78242b88ae06c2866a994
Carlos <carlostechsoftware@hotmail.com>
2024-11-06 12:44:16 -0300

    Commit inicial

---
+++ SchoolPayroll/src/test/java/project/SchoolPayroll/SchoolPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package project.SchoolPayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
